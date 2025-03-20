952fb458bd9877823798d8ce62a29a893567e2ad
LucasE. <90871939+Hybiu69@users.noreply.github.com>
2025-03-20 15:31:19 -0300

    Delete Farmacia directory

--- Farmacia/src/main/java/com/projeto1/repositorio/repDrogaria.java
+++
@@ -1,9 +0,0 @@
-package com.projeto1.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projeto1.entidade.Drogaria;
-
-public interface repDrogaria extends JpaRepository <Drogaria, Long> {
-
-}
--- Farmacia/src/main/java/com/projeto1/repositorio/repMedicamento.java
+++
@@ -1,9 +0,0 @@
-package com.projeto1.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projeto1.entidade.Medicamento;
-
-public interface repMedicamento extends JpaRepository <Medicamento,Long> {
-
-}
